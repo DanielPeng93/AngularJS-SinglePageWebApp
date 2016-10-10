e159c8b9d36da53b5df5074d39c09a84f895c8b8
DanielPeng93 <xinpeng@bu.edu>
2016-10-09 20:17:48 -0400

    commit 7

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-
 (function () {
 'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('NameCalculator', [])
 
-.controller('MyFirstController', function ($scope) {
+.controller('NameCaculatorController', function ($scope) {
   $scope.name = "";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+  $scope.totalValue = 0;
+
+  $scope.displayNumeric = function () {
+    var totalNameValue = calculatNumericForString($scope.name);
+    $scope.totalValue = totalNameValue;
   };
+
+
+  function calculatNumericForString(string) {
+    var totalStringValue = 0;
+    for (var i = 0; i < string.length; i++) {
+      totalStringValue += string.charCodeAt(i);
+    }
+
+    return totalStringValue;
+  }
+
 });
+
 
 })();
