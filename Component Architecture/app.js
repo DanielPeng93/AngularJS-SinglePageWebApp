4120039837477f2cf3d3b5e0b72744cf9536acd9
DanielPeng93 <xinpeng@bu.edu>
2016-10-26 22:26:14 -0400

    scope event

--- Component Architecture/app.js
+++ Component Architecture/app.js
@@ -1,9 +1,10 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListComponentApp', [])
+angular.module('ShoppingListEventsApp', [])
 .controller('ShoppingListController', ShoppingListController)
 .factory('ShoppingListFactory', ShoppingListFactory)
+.service('WeightLossFilterService', WeightLossFilterService)
 .component('shoppingList', {
   templateUrl: 'shoppingList.html',
   controller: ShoppingListComponentController,
@@ -12,49 +13,75 @@ angular.module('ShoppingListComponentApp', [])
     myTitle: '@title',
     onRemove: '&'
   }
+})
+.component('loadingSpinner', {
+  templateUrl: 'spinner.html',
+  controller: SpinnerController
 });
 
-ShoppingListComponentController.$inject = ['$scope', '$element']
-function ShoppingListComponentController($scope, $element) {
+
+SpinnerController.$inject = ['$rootScope']
+function SpinnerController($rootScope) {
   var $ctrl = this;
 
-  $ctrl.cookiesInList = function () {
-    for (var i = 0; i < $ctrl.items.length; i++) {
-      var name = $ctrl.items[i].name;
-      if (name.toLowerCase().indexOf("cookie") !== -1) {
-        return true;
-      }
-    }
+  var cancelListener = $rootScope.$on('shoppinglist:processing', function (event, data) {
+    console.log("Event: ", event);
+    console.log("Data: ", data);
 
-    return false;
+    if (data.on) {
+      $ctrl.showSpinner = true;
+    }
+    else {
+      $ctrl.showSpinner = false;
+    }
+  });
+
+  $ctrl.$onDestroy = function () {
+    cancelListener();
+  };
+
+};
+
+
+ShoppingListComponentController.$inject = ['$rootScope', '$element', '$q', 'WeightLossFilterService']
+function ShoppingListComponentController($rootScope, $element, $q, WeightLossFilterService) {
+  var $ctrl = this;
+  var totalItems;
+
+  $ctrl.$onInit = function () {
+    totalItems = 0;
+  };
+
+
+  $ctrl.$doCheck = function () {
+    if ($ctrl.items.length !== totalItems) {
+      totalItems = $ctrl.items.length;
+
+      $rootScope.$broadcast('shoppinglist:processing', {on: true});
+      var promises = [];
+      for (var i = 0; i < $ctrl.items.length; i++) {
+        promises.push(WeightLossFilterService.checkName($ctrl.items[i].name));
+      }
+
+      $q.all(promises)
+      .then(function (result) {
+        // Remove cookie warning
+        var warningElem = $element.find('div.error');
+        warningElem.slideUp(900);
+      })
+      .catch(function (result) {
+        // Show cookie warning
+        var warningElem = $element.find('div.error');
+        warningElem.slideDown(900);
+      })
+      .finally(function () {
+        $rootScope.$broadcast('shoppinglist:processing', { on: false });
+      });
+    }
   };
 
   $ctrl.remove = function (myIndex) {
     $ctrl.onRemove({ index: myIndex });
-  };
-
-  $ctrl.$onInit = function () {
-    console.log("We are in $onInit()");
-  };
-
-  $ctrl.$onChanges = function (changeObj) {
-    console.log("Changes: ", changeObj);
-  }
-
-  $ctrl.$postLink = function () {
-    $scope.$watch('$ctrl.cookiesInList()', function (newValue, oldValue) {
-      console.log($element);
-      if (newValue === true) {
-        // Show warning
-        var warningElem = $element.find('div.error');
-        warningElem.slideDown(900);
-      }
-      else {
-        // Hide warning
-        var warningElem = $element.find('div.error');
-        warningElem.slideUp(900);
-      }
-    });
   };
 }
 
@@ -123,6 +150,54 @@ function ShoppingListFactory() {
   };
 
   return factory;
+}
+
+
+WeightLossFilterService.$inject = ['$q', '$timeout']
+function WeightLossFilterService($q, $timeout) {
+  var service = this;
+
+  service.checkName = function (name) {
+    var deferred = $q.defer();
+
+    var result = {
+      message: ""
+    };
+
+    $timeout(function () {
+      // Check for cookies
+      if (name.toLowerCase().indexOf('cookie') === -1) {
+        deferred.resolve(result)
+      }
+      else {
+        result.message = "Stay away from cookies, Yaakov!";
+        deferred.reject(result);
+      }
+    }, 3000);
+
+    return deferred.promise;
+  };
+
+
+  service.checkQuantity = function (quantity) {
+    var deferred = $q.defer();
+    var result = {
+      message: ""
+    };
+
+    $timeout(function () {
+      // Check for too many boxes
+      if (quantity < 6) {
+        deferred.resolve(result);
+      }
+      else {
+        result.message = "That's too much, Yaakov!";
+        deferred.reject(result);
+      }
+    }, 1000);
+
+    return deferred.promise;
+  };
 }
 
 })();
